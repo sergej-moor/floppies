1d5bc96fa3795649af04c04da9705a9e0fa539b8
Sergej Moor <sergej.moor@stud.th-luebeck.de>
2023-10-25 22:40:43 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="bg-gray-300">
+	<div class="mx-4 min-h-screen">
+		<slot />
+	</div>
+</div>
---
+++ src/routes/floppies/+page.svelte
@@ -0,0 +1,94 @@
+<script lang="ts">
+	import P5, { type Sketch } from 'p5-svelte';
+	import { onMount } from 'svelte';
+	let artboard;
+	let sketch: Sketch;
+
+	let artboardWidth = 300;
+	let artboardHeight = 200;
+
+	sketch = (p5) => {
+		p5.setup = () => {
+			p5.createCanvas(artboardWidth, artboardHeight);
+		};
+
+		let colors = [
+			'#d9ed92',
+			'#b5e48c',
+			'#99d98c',
+			'#76c893',
+			'#52b69a',
+			'#34a0a4',
+			'#168aad',
+			'#1a759f',
+			'#1e6091',
+			'#184e77'
+		];
+		let yoffs = new Array(colors.length).fill(0.0);
+		p5.draw = () => {
+			p5.background(colors[9]);
+
+			for (let index = 0; index < colors.length; index++) {
+				const element = colors[index];
+				drawWave(element, index, 100, 0.1 + index * 0.02, 0.01, 15 * index, 10 );
+			}
+
+			/* drawWave('#252A34', 0, 40, 0.08, 0.01, -20);
+			drawWave('#08D9D6', 1, 120, 0.1, 0.02);
+			drawWave('#FF2E63', 1, 120, 0.1, 0.02, 50); */
+
+			function drawWave(
+				color: string,
+				yoffsindex: number,
+				height: number = 100,
+				xdim: number = 0.05,
+				ydim: number = 0.01,
+				yoffset: number = 0,
+				smoothness: number = 10
+			) {
+				p5.fill(color);
+				p5.noStroke();
+				// We are going to draw a polygon out of the wave points
+				p5.beginShape();
+
+				let xoff = 5; // Option #1: 2D Noise
+				// let xoff = yoff; // Option #2: 1D Noise
+
+				// Iterate over horizontal pixels
+				for (let x = 0; x <= p5.width; x += smoothness) {
+					// Calculate a y value according to noise, map to
+
+					let y = p5.map(p5.noise(xoff, yoffs[yoffsindex]), 0, 1, height, 20);
+					p5.vertex(x, y + yoffset -20);
+					// Increment x dimension for noise
+					xoff += xdim;
+				}
+				// increment y dimension for noise
+				yoffs[yoffsindex] += ydim;
+				p5.vertex(p5.width, p5.height);
+				p5.vertex(0, p5.height);
+				p5.endShape(p5.CLOSE);
+			}
+		};
+	};
+
+	onMount(async () => {});
+</script>
+
+<div class="justify-center flex p-8">
+	<div bind:this={artboard} class={`max-w-[${artboardWidth}] bg-gray-500`}>
+		<P5 {sketch} />
+	</div>
+</div>
+
+<!-- <script lang="ts">
+	let width: number;
+	let height: number;
+</script>
+
+Paragraph width: {width}px Paragraph height: {height}px
+
+<div class={`w-full h-[${width}px]`} bind:clientWidth={width} bind:clientHeight={height}>
+	This is some example text.
+</div>
+ -->
